e148003e6c8b39895db7cc12b4d97ee8a71009c9
shun <quxiangshun@gmail.com>
2022-04-10 20:17:03 +0800

    feat: 分类区块动态渲染数据

--- pages/index/components/category-box.vue
+++ pages/index/components/category-box.vue
@@ -1,17 +1,52 @@
 <template>
 	<view class="category-box">
-		<view>前端</view>
-		<view>前端</view>
-		<view>前端</view>
-		<view>前端</view>
-		<view>前端</view>
-		<view>前端</view>
-		<view>前端</view>
+		<!-- slice(开始下标, 结束小标)，返回指定范围的元素，包头不包尾（前闭后开） -->
+		<view v-for="(item, index) in categoryList.slice(0, 7)" :key="index">{{item.name}}</view>
 		<view>全部分类</view>
 	</view>
 </template>
 
 <script>
+	export default {
+		props: {
+			categoryList: {
+				type: Array,
+				default: () => [{
+						id: 1,
+						name: '京剧'
+					},
+					{
+						id: 2,
+						name: '黄梅戏'
+					},
+					{
+						id: 3,
+						name: '昆剧'
+					},
+					{
+						id: 4,
+						name: '沪剧'
+					},
+					{
+						id: 5,
+						name: '越剧'
+					},
+					{
+						id: 6,
+						name: '淮剧'
+					},
+					{
+						id: 7,
+						name: '小调'
+					},
+					{
+						id: 8,
+						name: '粤剧'
+					},
+				]
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
@@ -22,6 +57,7 @@
 		flex-wrap: wrap;
 		// 上右下左
 		padding: 20rpx 30rpx 0 30rpx;
+
 		>view {
 			width: 160rpx;
 			height: 70rpx;
