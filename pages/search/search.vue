7a18b146ec74f6ebd4c11f07c81451b7c2fd515d
shun <quxiangshun@gmail.com>
2022-04-21 15:47:54 +0800

    feat: search页面添加自动获取焦点

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -5,18 +5,37 @@
 </template>
 
 <script>
+	// 页面实例
+	let webView = null;
 	export default {
 		data() {
 			return {
-				
+				params: null
 			}
 		},
 		onLoad(option) {
-			// 接收参数
-			console.log('option', JSON.parse(option.params))
+			// #ifdef APP-PLUS
+			// 获取当前页面实例
+			webView = this.$scope.$getAppWebview();
+			// #endif 
+			
+			// 如果有参数，不自动获取焦点，没有参数，自动获取焦点
+			if(option.params) {
+				// 接收参数
+				// console.log('option', JSON.parse(option.params))
+				this.params = JSON.parse(option.params)
+				this.doSearch()
+			} else {
+				// #ifdef APP-PLUS
+				// 获取焦点
+				webView.setTitleNViewSearchInputFocus(true)
+				// #endif 
+			}
 		},
 		methods: {
-			
+			doSearch() {
+				uni.showLoading()
+			}
 		}
 	}
 </script>
