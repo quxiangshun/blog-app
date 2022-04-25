6a54be205f4f203508210a4e6d17b3f56277ec75
shun <quxiangshun@gmail.com>
2022-04-25 20:17:10 +0800

    feat: 添加课程详情中的章节组件

--- pages/course/components/course-dir.vue
+++ pages/course/components/course-dir.vue
@@ -1,11 +1,79 @@
 <template>
-	<view>
-		<view v-for="i in 100" :key="i">课程章节介绍-{{i}}</view>
+	<view class="course-dir">
+		<view v-for="(item, index) in chapterList" :key="index">
+			<text class="chapters text-ellipsis" style="-webkit-line-clamp: 1;">第{{index + 1}}章 {{item.name}}</text>
+			<!-- 第几节 -->
+			<view class="sections row" v-for="(section, si) in item.sectionList" :key="si">
+				<text class="iconfont icon-roundrightfill"></text>
+				<view class="row">
+					<text>{{index + 1}}-{{si + 1}}</text>
+					<text class="title text-ellipsis" style="-webkit-line-clamp: 1;">{{section.name}}</text>
+				</view>
+				<text v-if="section.isFree" class="see">试看</text>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	export default {
+		props: {
+			chapterList: {
+				type: Array,
+				default: () => [{
+					id: 1,
+					name: '项目内容介绍',
+					sectionList: [{
+							id: 1,
+							name: '什么是黄梅戏',
+							isFree: 1, // 是否付费 0收费，1免费
+						},
+						{
+							id: 2,
+							name: '黄梅戏腔调',
+							isFree: 0, // 是否付费 0收费，1免费
+						}
+					]
+				}]
+			}
+		}
+	}
 </script>
 
-<style>
+<style lang="scss">
+	.course-dir {
+		padding: 0 36rpx;
+
+		.chapters {
+			color: $jh-text-color-black;
+			font-size: 35rpx;
+			font-weight: bold;
+			margin-top: 30rpx;
+			margin-bottom: 10rpx;
+		}
+
+		.sections {
+			color: #3D3C40;
+			font-size: 30rpx;
+			border-bottom: $jh-underline;
+			padding: 20rpx 0;
+			line-height: 40rpx;
+
+			text {
+				margin-right: 10rpx;
+			}
+
+			.title {
+				width: 470rpx;
+			}
+
+			.see {
+				color: $jh-text-color-blue;
+				font-size: 25rpx;
+				// 靠右  flex: 1 表示把区域平分
+				flex: 1;
+				text-align: right;
+			}
+		}
+	}
 </style>
