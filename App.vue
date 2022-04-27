551b58014acbb86b1c856f02b19dbe807998cfbe
shun <quxiangshun@gmail.com>
2022-04-27 21:28:54 +0800

    feat: subNView原生子窗体渲染事项-配置视频播放页面

--- App.vue
+++ App.vue
@@ -18,8 +18,10 @@
 	/* 公共样式 */
 	@import url("@/common/css/common.css");
 	
+	/* #ifndef APP-PLUS-NVUE */
 	/* 导入图标，官方建议使用本地方式~@开头 */
 	@import url("~@/static/icon/iconfont.css");
 	/* 微信小程序开发工具不支持网络链接，只支持本地方式。打包上线时，只支持在线方式且使用https://开头的网络文件 */
 	/* @import url("https://at.alicdn.com/t/font_xxxx.css"); */
+	/* #endif */
 </style>
---
+++ pages/course/course-play.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>嘻嘻嘻</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
