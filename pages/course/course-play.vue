cd342ddef7c0e68ac8b50d1bc19ed4b53b91cbe5
shun <quxiangshun@gmail.com>
2022-04-28 17:14:26 +0800

    feat: 添加非app端的视频播放器

--- pages/course/course-play.vue
+++ pages/course/course-play.vue
@@ -1,8 +1,31 @@
 <template>
-	<view>嘻嘻嘻</view>
+	<view class="course-play">
+		<!-- #ifndef APP-PLUS -->
+		<video id="myVideo" style="width: 750rpx; height: 423rpx;" :poster="videoMidea.mainImage"
+			:src="videoMidea.videoUrl"></video>
+		<!-- #endif -->
+	</view>
 </template>
 
 <script>
+	let videoContext = null
+	export default {
+		data() {
+			return {
+				videoMidea: {
+					id: 1,
+					title: '1-1 项目需求分析项目需求',
+					mainImage: 'https://pics6.baidu.com/feed/0e2442a7d933c895ff4183829e8c4cfa830200b0.jpeg?token=02d84b8835896cf4af92fdd5fc822d8d',
+					videoUrl: 'https://vd3.bdstatic.com/mda-jkkgqi4rmyigr1gh/sc/mda-jkkgqi4rmyigr1gh.mp4',
+				},
+			}
+		},
+		onReady() {
+			// #ifndef MP-ALIPAY
+			videoContext = uni.createVideoContext('myVideo', this)
+			// #endif
+		},
+	}
 </script>
 
 <style>
