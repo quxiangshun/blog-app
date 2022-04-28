b097b795dca9fa77ac4dbdcf92e9c7c1d9e5ce33
shun <quxiangshun@gmail.com>
2022-04-28 17:45:48 +0800

    feat: 视频播放添加标题、章节列表以及分享评价按钮

--- pages/course/course-play.vue
+++ pages/course/course-play.vue
@@ -4,12 +4,39 @@
 		<video id="myVideo" style="width: 750rpx; height: 423rpx;" :poster="videoMidea.mainImage"
 			:src="videoMidea.videoUrl"></video>
 		<!-- #endif -->
+		
+		<!-- 课程标题与详情按钮 -->
+		<view class="space-between course-info">
+			<text class="title">标题</text>
+			<view class="right">
+				<text>详情</text>
+				<text class="iconfont icon-right"></text>
+			</view>
+		</view>
+		<!-- 课程列表 -->
+		<course-dir :isBuy="true"></course-dir>
+		
+		<!-- 底部按钮 -->
+		<view class="bottom center">
+			<view class="btn-item one column">
+				<text class="iconfont icon-Share-Outline"></text>
+				<text>分享</text>
+			</view>
+			<view class="btn-item one column">
+				<text class="iconfont icon-edit"></text>
+				<text>评价</text>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	let videoContext = null
+	import courseDir from './components/course-dir.vue'
 	export default {
+		components: {
+			courseDir
+		},
 		data() {
 			return {
 				videoMidea: {
@@ -20,7 +47,7 @@
 				},
 			}
 		},
-		onReady() {
+		onReady(option) {
 			// #ifndef MP-ALIPAY
 			videoContext = uni.createVideoContext('myVideo', this)
 			// #endif
@@ -28,5 +55,41 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.course-info {
+		padding: 30rpx;
+		font-weight: bold;
+		border-bottom: $jh-underline;
+		font-size: 38rpx;
+		
+		.title {
+			width: 500rpx;
+			font-size: 35rpx;
+			line-height: 60rpx;
+			color: $jh-text-color-black;
+		}
+		.right>text {
+			font-size: 28rpx;
+			line-height: 60rpx;
+			color: $jh-text-color-black;
+		}
+	}
+	.bottom {
+		position: fixed;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: #FFFFFF;
+		border-top: $jh-underline;
+		.btn-item {
+			text {
+				text-align: center;
+				color: $jh-text-color-grey;
+				font-size: 25rpx;
+			}
+		}
+		.iconfont {
+			font-size: 38rpx !important;
+		}
+	}
 </style>
