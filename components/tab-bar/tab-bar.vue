4b99b7f6d636941e6f7c731494738ebab35b7d9d
shun <quxiangshun@gmail.com>
2022-05-03 15:50:10 +0800

    feat: 优化tab，添加滚动
    1. tabbar小于五个的时候采用默认宽度，大于等于五个采用滚动方式
    2. 滚动方式，点击tab后自动居中

--- components/tab-bar/tab-bar.vue
+++ components/tab-bar/tab-bar.vue
@@ -1,16 +1,18 @@
 <template>
 	<!-- @touchmove.stop.prevent="()=>{}"防止抖动 -->
 	<view class="tab-bar" @touchmove.stop.prevent="()=>{}">
-		<view class="bar-view center">
-			<view class="bar-item" :class="{active: index === value}" v-for="(item, index) in tabs" :key="index"
-				@click="changeTab(index)">
+		<scroll-view class="bar-view noScorll" scroll-x="true" :scroll-with-animation="true" :scroll-left="scrollLeft">
+			<view class="bar-item" :style="{width: `${itemWidth}px`}" :class="{active: index === value}"
+				v-for="(item, index) in tabs" :key="index" @click="changeTab(index)">
 				{{item.name}}
 			</view>
-		</view>
+		</scroll-view>
 	</view>
 </template>
 
 <script>
+	// 定时器变量
+	let timer = null
 	export default {
 		props: {
 			// 此处声明value，而不用index或者其他属性表示，因为要使用vue的语法糖，v-model双向绑定：
@@ -36,12 +38,46 @@
 				]
 			}
 		},
+		watch: {
+			tabs: {
+				immediate: true,
+				handler(newVal) {
+					// 监听标签选项数组，小于5个，平均分配每个的宽度
+					if (newVal && newVal.length < 5) {
+						this.itemWidth = this.windowWidth / newVal.length
+					}
+				}
+			}
+		},
+		data() {
+			return {
+				scrollLeft: 0, // 滚动条便宜位置（计算左右移动距离）
+				windowWidth: uni.getSystemInfoSync().windowWidth, // 获取窗口宽度(单位：px)
+				itemWidth: uni.upx2px(150), // 每个tab的宽度，默认是150（单位是rpx）,uni.upx2px(150)转成px
+			}
+		},
 		methods: {
 			changeTab(index) {
 				// 改变tab，点击不同的标签才更改值
 				if (this.value !== index) {
 					// 1. props声明value；2. 修改它时触发input事件传递
-					this.$emit("input", index)
+					if (timer) {
+						// 多次切换只执行最后一次
+						clearTimeout(timer)
+						timer = null
+					}
+					// 获取点击的这个距离左侧宽度
+					let width = this.itemWidth * (index + 1)
+					// 延迟400ms，以防用户切换太快
+					timer = setTimeout(() => {
+						// 点击的tab距离>屏幕的1/2（中间），则移动到中间
+						if (width - this.itemWidth / 2 > this.windowWidth / 2) {
+							this.scrollLeft = width - this.itemWidth / 2 - this.windowWidth / 2
+						} else {
+							this.scrollLeft = 0
+						}
+						this.$emit("input", index)
+					}, 200)
 				}
 			}
 		}
@@ -53,16 +89,16 @@
 		width: 100%;
 		height: 80rpx;
 		background-color: #FFFFFF;
-		// background-color: red;
 		border-bottom: 1rpx solid #EFEFF4;
 
 		.bar-view {
 			width: 100%;
+			white-space: nowrap;
 			text-align: center;
 
 			.bar-item {
-				// flex: 1 表示把区域平均分配
-				flex: 1;
+				display: inline-block;
+				width: 150rpx;
 				font-size: 30rpx;
 				// line-height和外层height一样就会上下居中
 				line-height: 80rpx;
