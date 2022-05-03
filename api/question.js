84b0258417fa0a6be2f431187d058bd2d062d8e8
shun <quxiangshun@gmail.com>
2022-05-03 21:10:47 +0800

    feat: 问答列表以及接入数据接口

--- api/question.js
+++ api/question.js
@@ -17,5 +17,53 @@ export default {
 				size
 			}
 		})
+	},
+	
+	/**
+	 * 分页查询热门回答列表接口(按照回复数降序)
+	 * @param {Object} current 当前页码
+	 * @param {Object} size 每页显示多少条
+	 */
+	getHotList(current = 1, size = 20) {
+		return request({
+			url: '/question/api/question/hot',
+			method: 'POST',
+			data: {
+				current,
+				size
+			}
+		})
+	},
+	
+	/**
+	 * 分页查询最新问题列表（按照更新时间排序）
+	 * @param {Object} current 当前页码
+	 * @param {Object} size 每页显示多少条
+	 */
+	getNewList(current = 1, size = 20) {
+		return request({
+			url: '/question/api/question/new',
+			method: 'POST',
+			data: {
+				current,
+				size
+			}
+		})
+	},
+	
+	/**
+	 * 分页查询最新问题列表接口（按更新时间降序）
+	 * @param {Object} current 当前页码
+	 * @param {Object} size 每页显示多少条
+	 */
+	getWaitList(current = 1, size = 20) {
+		return request({
+			url: '/question/api/question/wait',
+			method: 'POST',
+			data: {
+				current,
+				size
+			}
+		})
 	}
 }
--- pages/question/question.vue
+++ pages/question/question.vue
@@ -1,22 +1,69 @@
 <template>
-	<view>
-		
+	<view class="container">
+		<tab-bar ref="tabBar" :tabs="tabs" v-model="tabIndex"></tab-bar>
+		<swiper :style="{height: height}" :current="tabIndex" @change="swiperChange">
+			<swiper-item v-for="(tab,i) in tabs" :key="i">
+				<mescroll-item ref="mescrollItem" :i="i" :index="tabIndex" :tabs="tabs" :height="height">
+				</mescroll-item>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
+	import MescrollItem from "./components/question-swiper-item.vue";
+	import courseApi from '@/api/course.js'
+
 	export default {
+		components: {
+			MescrollItem
+		},
 		data() {
 			return {
-				
+				tabIndex: 0,
+				tabs: [
+					{
+						id: '1',
+						name: '热门回答'
+					},
+					{
+						id: '2',
+						name: '最新问题'
+					},
+					{
+						id: '1',
+						name: '等待回答'
+					}
+					
+				],
+				height: '400px'
 			}
 		},
+		onLoad() {
+			// 需要固定swiper的高度 (需减去悬浮tabs的高度64rpx)
+			this.height = uni.getSystemInfoSync().windowHeight - uni.upx2px(64) + 'px'
+		},
+		/**
+		 * 监听搜索框点击事件
+		 */
+		onNavigationBarSearchInputClicked() {
+			// 通过封装的混合文件，跳转到搜索页
+			this.navTo('/pages/search/search')
+		},
 		methods: {
-			
+			// 轮播菜单
+			swiperChange(e) {
+				this.tabIndex = e.detail.current
+				// 左右切换重新计算移动宽度
+				this.$refs.tabBar.changeTab(this.tabIndex)
+			},
 		}
 	}
 </script>
 
 <style>
-
+	page, .container {
+		height: 100%;
+		overflow: hidden;
+	}
 </style>
