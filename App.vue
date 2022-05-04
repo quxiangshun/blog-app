b5e519792ba9816eee8971bd2f07b084d3a59fc0
shun <quxiangshun@gmail.com>
2022-05-04 13:43:45 +0800

    feat: 我的页面
    1. 引入彩色图标
    2. 添加list组件
    3. 设置我的页面的头部样式

--- App.vue
+++ App.vue
@@ -24,4 +24,7 @@
 	/* 微信小程序开发工具不支持网络链接，只支持本地方式。打包上线时，只支持在线方式且使用https://开头的网络文件 */
 	/* @import url("https://at.alicdn.com/t/font_xxxx.css"); */
 	/* #endif */
+	
+	/* 彩色图标 */
+	@import url("~@/static/icon/iconfont-app-icon.css");
 </style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,6 +1,27 @@
 <template>
 	<view>
-		
+		<!-- 头部空出的距离 -->
+		<view class="status-bar">
+
+		</view>
+		<view class="my-header">
+			<view class="header-content center space-between">
+				<view class="left center">
+					<image class="header-image" :src="isLogin ? '' : '/static/logo.png'" mode=""></image>
+					<view v-if="isLogin" class="header-info column">
+						<text class="nickname">线程池</text>
+						<text class="username">用户名：休息休息吧</text>
+					</view>
+					<view v-else class="header-info column">
+						<text class="nickname">请登录</text>
+					</view>
+				</view>
+				<text class="iconfont icon-right"></text>
+			</view>
+		</view>
+
+		<!-- 功能列表 -->
+		<jh-list></jh-list>
 	</view>
 </template>
 
@@ -8,15 +29,55 @@
 	export default {
 		data() {
 			return {
-				
+				isLogin: false,
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.status-bar {
+		height: calc(var(--status-bar-height) + 48px);
+		background-color: $jh-color-primary;
+	}
 
+	.my-header {
+		background-color: $jh-color-primary;
+
+		.header-content {
+			padding: 50rpx 39rpx;
+			background-color: #FFFFFF;
+			border-radius: 30rpx 30rpx 0 0;
+		}
+
+		.left {
+			.header-image {
+				width: 150rpx;
+				height: 150rpx;
+				border-radius: 75rpx;
+			}
+
+			.header-info {
+				margin-left: 30rpx;
+
+				.nickname {
+					font-size: 39rpx;
+					font-weight: bold;
+				}
+
+				.username {
+					font-size: 33rpx;
+					color: $jh-text-color-grey;
+				}
+			}
+		}
+
+		.icon-right {
+			font-size: 35rpx;
+			color: $jh-text-color-grey;
+		}
+	}
 </style>
